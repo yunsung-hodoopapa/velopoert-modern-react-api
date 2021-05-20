b83b5b6f8c70127d048e6b20aa541153a3f5949c
yunsungyang-omc <vappet3477@gmail.com>
2021-05-20 15:14:31 +0900

    clone project:velopert modern react api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Users from './User';
+import { UsersProvider } from './UsersContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UsersProvider>
+      <Users />;
+    </UsersProvider>
   );
 }
 
